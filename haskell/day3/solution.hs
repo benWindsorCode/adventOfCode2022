00dc3a7a889fa0e9e38ca840149fd65c8ad15dd0
Ben Windsor <benjaminwindsor@hotmail.co.uk>
2022-12-03 15:10:25 +0000

    day3 haskell

---
+++ haskell/day3/solution.hs
@@ -0,0 +1,84 @@
+import Debug.Trace
+import Data.Char
+import Data.List
+
+main = do
+    content <- readFile ("./input.txt")
+    let contentLines = lines content
+    print $ part1 contentLines
+    print $ part2 contentLines
+
+part1 :: [String] -> Int
+part1 lines = sum (map scoreRucksack lines)
+
+part2 :: [String] -> Int
+part2 lines = part2Inner (map uniqueChars lines) 0
+
+part2Inner :: [String] -> Int -> Int
+part2Inner (first:second:third:xs) score = part2Inner xs (score + getPriority common)
+    where
+        common = commonItem first second third
+part2Inner [] score = score
+
+-- given three rucksacks find the common item
+commonItem (x:xs) second third 
+    | isCharInBothStrings x second third = x
+    | not $ isCharInBothStrings x second third = commonItem xs second third
+commonItem [] second thirs = error "rucksacks have no item in common"
+
+-- iterate over rucksack, each char in first half gets a score if its also in second half
+scoreRucksack rucksack = scoreRucksackInner (uniqueChars $ firstHalf rucksack) (uniqueChars $ secondHalf rucksack) 0
+
+scoreRucksackInner (x:xs) half score = scoreRucksackInner xs half (score + (scoreChar x half))
+scoreRucksackInner [] _ score = score
+
+-- a char gets a score of it is in the rucksack 
+scoreChar :: Char -> String -> Int
+scoreChar char string 
+    | isCharInString char string = getPriority char
+    | not $ isCharInString char string = 0
+
+-- return just a string of the unique chars in a string
+uniqueChars string = uniqueCharsInner string ""
+
+uniqueCharsInner (x:xs) chars 
+    | isCharInString x chars = uniqueCharsInner xs chars
+    | not $ isCharInString x chars = uniqueCharsInner xs (x:chars)
+uniqueCharsInner [] chars = chars
+
+isCharInBothStrings :: Char -> String -> String -> Bool
+isCharInBothStrings char string1 string2 = (isCharInString char string1) && (isCharInString char string2)
+
+isCharInString :: Char -> (String -> Bool)
+isCharInString char = any (==char)
+
+-- convert a-z and A-Z to their respective priorities
+getPriority :: Char -> Int
+getPriority char 
+    | isUpper char = upperScores !! (fromJust $ findIndex (==char) upper)
+    | not $ isUpper char = lowerScores !! (fromJust $ findIndex (==char) lower)
+    where
+        lower = ['a'..'z']
+        upper = ['A'..'Z']
+        lowerScores = [1..26]
+        upperScores = [27..52]
+
+firstHalf :: String -> String
+firstHalf input = slice start mid input
+    where
+        total = length input
+        mid = total `div` 2
+        start = 0
+
+secondHalf input = slice mid total input
+    where
+        total = length input
+        mid = total `div` 2
+        start = 0
+
+slice start end = take (end - start) . drop start
+
+-- Borrowed from here as couldnt work out the import https://hackage.haskell.org/package/strict-0.3.2/docs/Data-Strict-Maybe.html#v%3AfromJust
+fromJust :: Maybe a -> a
+fromJust Nothing  = error "Data.Strict.Maybe.fromJust: Nothing"
+fromJust (Just x) = x
