68018788aebe1bc080a3f83a98006ac3e75a179c
Ben Windsor <benjaminwindsor@hotmail.co.uk>
2022-12-06 07:25:26 +0000

    cleanup day 6

--- haskell/day6/solution.hs
+++ haskell/day6/solution.hs
@@ -4,18 +4,17 @@ import Debug.Trace
 main :: IO ()
 main = do
     content <- readFile "./input.txt"
-    let contentLines = lines content
-    print content
     print $ packetStartCount content
     print $ messageStartCount content
 
 messageStartCount packet | length packet >= 14 = messageStartCountInner packet 14
+messageStartCount _ = error "Packet not long enough to calculate start of message marker"
 
 messageStartCountInner packet count | listUnique (take 14 packet) = count
 messageStartCountInner packet count = messageStartCountInner (tail packet) (count + 1)
 
 packetStartCount packet | length packet >= 4 = packetStartCountInner packet 4
-packetStartCount _ = error "Packet not long enough to calculate"
+packetStartCount _ = error "Packet not long enough to calculate start of packet marker"
 
 packetStartCountInner (a:b:c:d:xs) count | listUnique [a, b, c, d] = count
 packetStartCountInner (a:b:c:d:xs) count = packetStartCountInner (b:c:d:xs) (count + 1)
